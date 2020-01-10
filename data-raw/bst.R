64b82c19ec68ea8e39dc4e659314cceb3be8388d
mkearney <kearneymw@missouri.edu>
2020-01-09 20:16:26 -0600

    Rm data-raw

--- data-raw/bst.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `bst` dataset goes here
-
-xgb_counts_bot <- tfse::read_RDS("xgb-counts-model.rds")
-xgb_props_bot <- tfse::read_RDS("xgb-props-model.rds")
-source_types <- tfse::read_RDS("source_types.rds")
-
-usethis::use_data(source_types, xgb_counts_bot, xgb_props_bot,
-  overwrite = TRUE, internal = TRUE)
