42d6dbda390f9345d69ed46514f3da4658ddfe32
mkearney <kearneymw@missouri.edu>
2020-01-21 12:15:20 -0600

    Edit test-botometer

--- tests/testthat/test-botometer.R
+++ tests/testthat/test-botometer.R
@@ -3,6 +3,6 @@ test_that("predict_botometer works", {
   b <- predict_botometer(c("kearneymw", "jack", NA_character_, "kearneymw", "a2431sdfas1234dfasdfa"))
   expect_true(is.data.frame(b))
   expect_equal(nrow(b), 5L)
-  expect_equal(ncol(b), 5L)
+  expect_equal(ncol(b), 4L)
   expect_named(b, expected = c("user_id", "screen_name", "botometer_english", "botometer_universal"))
 })
