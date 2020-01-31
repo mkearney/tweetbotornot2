d8dd5258c1e14f73bbfb4008f868974a5c446d50
mkearney <kearneymw@missouri.edu>
2020-01-30 18:20:28 -0600

    Edit Imports

--- tests/testthat/test-botometer.R
+++ tests/testthat/test-botometer.R
@@ -1,6 +1,8 @@
 test_that("predict_botometer works", {
   skip_on_cran()
-  b <- predict_botometer(c("kearneymw", "jack", NA_character_, "kearneymw", "a2431sdfas1234dfasdfa"))
+  token <- readRDS("twitter_tokens")
+  b <- predict_botometer(c("kearneymw", "jack", NA_character_, "kearneymw", "a2431sdfas1234dfasdfa"),
+    token = token)
   expect_true(is.data.frame(b))
   expect_equal(nrow(b), 5L)
   expect_equal(ncol(b), 4L)
